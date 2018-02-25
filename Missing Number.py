28c38a14ea68a04a7d72148bd383b27789260fc6
hecter <hecter654321@gmail.com>
2018-02-25 17:18:39 +0800

    pass testcase: testSample by using math

--- Missing Number.py
+++ Missing Number.py
@@ -5,11 +5,20 @@ class unitest(unittest.TestCase):
         Input = []
         Output = 0
         self.assertEqual(Solution().missingNumber(Input),Output)
+    def testSample(self):
+        Input = [3,0,1]
+        Output = 2
+        self.assertEqual(Solution().missingNumber(Input),Output)
 
 class Solution():
     def missingNumber(self, nums):
         if nums == []:
             return 0
+        total = 0
+        for i in nums:
+            total += i
+        Sum = (1 + len(nums)) * len(nums) / 2
+        return int(Sum) - total
 
 if __name__ == '__main__':
     unittest.main()
